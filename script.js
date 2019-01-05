bed7cc14f59c076f94639daa0af4537cf8ce84d3
Filip Blajiu <filipblajiu@gmail.com>
2019-01-05 05:33:34 +0200

    Țfinished schedule and updates on page load

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ let transport= {
                 "station": "Râșnov",
                 "departure": {
                     0: [6,28],
-                    1: [8,09],
+                    1: [8,9],
                     2: [9,44],
                     3: [12,30],
                     4: [14,44],
@@ -97,7 +97,7 @@ let transport= {
                     5: [16,52],
                     6: [18,28],
                     7: [20,49],
-                    8: [23,04]
+                    8: [23,4]
                 }
             },
             7: {
@@ -111,7 +111,7 @@ let transport= {
                     5: [16,56],
                     6: [18,32],
                     7: [20,53],
-                    8: [23,08]
+                    8: [23,8]
                 }
             },
             8: {
@@ -119,9 +119,9 @@ let transport= {
                 "departure": {
                     0: [6,45],
                     1: [8,25],
-                    2: [10,00],
+                    2: [10,0],
                     3: [12,46],
-                    4: [15,00],
+                    4: [15,0],
                     5: [16,59],
                     6: [18,35],
                     7: [20,56],
@@ -136,13 +136,13 @@ let transport= {
             0: {
                 "station": "Zărnești",
                 "departure": {
-                    0: [5,00],
+                    0: [5,0],
                     1: [6,8],
                     2: [6,52],
                     3: [8,32],
                     4: [13,10],
-                    5: [15,05],
-                    6: [17,05],
+                    5: [15,5],
+                    6: [17,5],
                     7: [18,40],
                     8: [21,25]
                 }
@@ -150,113 +150,112 @@ let transport= {
             1: {
                 "station": "G-ral Moșoiu",
                 "departure": {
-                    0: [6,42],
-                    1: [8,22],
-                    2: [9,57],
-                    3: [12,43],
-                    4: [14,57],
-                    5: [16,56],
-                    6: [18,32],
-                    7: [20,53],
-                    8: [23,08]
+                    0: [5,4],
+                    1: [6,12],
+                    2: [6,56],
+                    3: [8,36],
+                    4: [13,14],
+                    5: [15,9],
+                    6: [17,9],
+                    7: [18,44],
+                    8: [21,29]
                 }
             },
             2: {
                 "station": "Tohanu Vechi",
                 "departure": {
-                    0: [6,38],
-                    1: [8,18],
-                    2: [9,53],
-                    3: [12,39],
-                    4: [14,53],
-                    5: [16,52],
-                    6: [18,28],
-                    7: [20,49],
-                    8: [23,04]
+                    0: [5,7],
+                    1: [6,15],
+                    2: [6,59],
+                    3: [8,39],
+                    4: [13,17],
+                    5: [15,12],
+                    6: [17,12],
+                    7: [18,47],
+                    8: [21,32]
                 }
             },
             3: {
                 "station": "Râșnov H.",
                 "departure": {
-                    0: [6,32],
-                    1: [8,12],
-                    2: [9,47],
-                    3: [12,33],
-                    4: [14,47],
-                    5: [16,46],
-                    6: [18,22],
-                    7: [20,43],
-                    8: [22,58]
+                    0: [5,13],
+                    1: [6,21],
+                    2: [7,5],
+                    3: [8,45],
+                    4: [13,23],
+                    5: [15,18],
+                    6: [17,18],
+                    7: [18,53],
+                    8: [21,38]
                 }
             },
             4: {
                 "station": "Râșnov",
                 "departure": {
-                    0: [6,28],
-                    1: [8,09],
-                    2: [9,44],
-                    3: [12,30],
-                    4: [14,44],
-                    5: [16,43],
-                    6: [18,19],
-                    7: [20,40],
-                    8: [22,55]
+                    0: [5,17],
+                    1: [6,25],
+                    2: [7,9],
+                    3: [8,49],
+                    4: [13,27],
+                    5: [15,22],
+                    6: [17,22],
+                    7: [18,57],
+                    8: [21,42]
                 }
             },
             5: {
                 "station": "Cristian",
                 "departure": {
-                    0: [6,13],
-                    1: [8,4],
-                    2: [9,39],
-                    3: [12,25],
-                    4: [14,39],
-                    5: [16,38],
-                    6: [18,14],
-                    7: [20,35],
-                    8: [22,50]
+                    0: [5,22],
+                    1: [6,30],
+                    2: [7,14],
+                    3: [8,54],
+                    4: [13,32],
+                    5: [15,27],
+                    6: [17,27],
+                    7: [19,2],
+                    8: [21,47]
                 }
             },
             6: {
                 "station": "Lustic H.",
                 "departure": {
-                    0: [6,8],
-                    1: [8,0],
-                    2: [9,35],
-                    3: [12,21],
-                    4: [14,35],
-                    5: [16,34],
-                    6: [18,10],
-                    7: [20,31],
-                    8: [22,46]
+                    0: [5,26],
+                    1: [6,34],
+                    2: [7,18],
+                    3: [8,58],
+                    4: [15,31],
+                    5: [17,31],
+                    6: [19,6],
+                    7: [21,51]
                 }
             },
             7: {
                 "station": "Bartolomeu",
                 "departure": {
-                    0: [6,0],
-                    1: [7,52],
-                    2: [9,26],
-                    3: [12,12],
-                    4: [14,27],
-                    5: [16,26],
-                    6: [18,1],
-                    7: [20,22],
-                    8: [22,37]
+                    0: [5,34],
+                    1: [6,43],
+                    2: [7,28],
+                    3: [9,6],
+                    4: [13,43],
+                    5: [15,39],
+                    6: [17,39],
+                    7: [19,14],
+                    8: [21,59]
                 }
             },
             8: {
                 "station": "Brașov",
                 "departure": {
-                    0: [5,54],
-                    1: [7,45],
-                    2: [9,20],
-                    3: [12,5],
-                    4: [14,20],
-                    5: [16,20],
-                    6: [17,55],
-                    7: [20,15],
-                    8: [22,30]
+                    0: [5,40],
+                    1: [6,48],
+                    2: [7,33],
+                    3: [9,11],
+                    4: [13,49],
+                    5: [15,45],
+                    6: [17,45],
+                    7: [19,20],
+                    8: [22,6]
                 }
             },
         }
@@ -282,11 +281,19 @@ function stations(reverse){
     if(reverse){
         let r = station.slice().reverse();
         for(let i = 0; i < station.length; i++){
-            s.push(`<input type="radio" name="station" id="${i}"><label for="${i}">${r[i]}</label>`);
+            if(i == 0){
+                s.push(`<input type="radio" name="station" id="${i}" checked><label for="${i}">${r[i]}</label>`);
+            } else {
+                s.push(`<input type="radio" name="station" id="${i}"><label for="${i}">${r[i]}</label>`);
+            }
         }
     } else {
         for(let i = 0; i < station.length; i++){
-            s.push(`<input type="radio" name="station" id="${i}"><label for="${i}">${station[i]}</label>`);
+            if(i == 0){
+                s.push(`<input type="radio" name="station" id="${i}" checked><label for="${i}">${station[i]}</label>`);
+            } else {
+                s.push(`<input type="radio" name="station" id="${i}"><label for="${i}">${station[i]}</label>`);
+            }
         }
         
     }
@@ -328,6 +335,26 @@ function whatTime(stops){
 
 
 function update(){
+    let next,
+    current,
+    previous;
+    
+    let stops = transport[rute].stations[0].departure;
+    console.log(stops);
+    current = whatTime(stops);
+
+    let h = stops[current][0];
+    let m = stops[current][1];    
+
+    if(h < 10){
+        h = "0" + h;
+    }
+    if(m < 10){
+        m = "0" + m;
+    }
+
+    document.getElementById("train").innerHTML = h + ":" + m;
+
     document.getElementsByName("station").forEach(station => {
         station.addEventListener("click", function(){
     
@@ -335,13 +362,23 @@ function update(){
             console.log(stops);
             current = whatTime(stops);
     
+            let h = stops[current][0];
+            let m = stops[current][1];    
+    
+            if(h < 10){
+                h = "0" + h;
+            }
+            if(m < 10){
+                m = "0" + m;
+            }
+    
+            document.getElementById("train").innerHTML = h + ":" + m;
             
             document.getElementById("next").addEventListener("click", function(){
     
                 let k = Object.keys(stops);
                 previous = current;
                 current = parseInt(current)+1 >= k.length ? 0 : parseInt(current)+1;
-                console.log(current);
                 next = parseInt(current)+2 >= k.length ? 0 : parseInt(current)+2;
                 
                 let h = stops[current][0];
@@ -376,17 +413,7 @@ function update(){
                 document.getElementById("train").innerHTML = h + ":" + m;
             });
             
-            let h = stops[current][0];
-            let m = stops[current][1];    
-    
-            if(h < 10){
-                h = "0" + h;
-            }
-            if(m < 10){
-                m = "0" + m;
-            }
-    
-            document.getElementById("train").innerHTML = h + ":" + m;
+            l
         });
         
     });
